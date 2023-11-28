a5195146f5974bda92f72c2fb2b86d6a498a941d
Etienne de Montalivet <etienne.demontalivet@wysscenter.ch>
2023-11-28 13:39:24 +0100

    feat: decode markers packet

--- micromed_io/tcp.py
+++ micromed_io/tcp.py
@@ -1,11 +1,12 @@
 """Micromed TCP module."""
 
-from enum import IntEnum
 import sys
+from enum import IntEnum
 
 sys.path.append("..")
-from micromed_io.in_out import MicromedIO
 import logging
+
+from micromed_io.in_out import MicromedIO
 
 
 class MicromedPacketType(IntEnum):
@@ -14,6 +15,7 @@ class MicromedPacketType(IntEnum):
     HEADER = 0
     EEG_DATA = 1
     NOTE = 2
+    MARKER = 3
 
 
 def decode_tcp_header_packet(packet: bytearray):
@@ -41,3 +43,22 @@ def decode_tcp_header_packet(packet: bytearray):
     else:
         logging.warning(f"header empty packet: [{packet.decode()}]")
     return packet_type, next_packet_size
+
+
+def decode_tcp_markers_packet(packet: bytearray):
+    """Decode the Micromed tcp markers packet.
+
+    Parameters
+    ----------
+    packet : bytearray
+        The tcp packet to decode.
+
+    Returns
+    -------
+    tuple
+        trigger_sample: the sample when the marker is received
+        trigger_value: trigger value
+    """
+    trigger_sample = int.from_bytes(packet[:4], byteorder="little")
+    trigger_value = int.from_bytes(packet[4:6], byteorder="little")
+    return trigger_sample, trigger_value
