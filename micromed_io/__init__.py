0e85670b8cfadaf5399299d45c2f33dc2c512e65
etiennedemontalivet <etienne.demontalivet@protonmail.com>
2024-04-10 12:54:01 +0200

    repo: V0.4.3

--- micromed_io/__init__.py
+++ micromed_io/__init__.py
@@ -1,2 +1,3 @@
 """Micromed IO module."""
-__version__ = "0.4.2"
+
+__version__ = "0.4.3"
--- tests/test_micromed_io.py
+++ tests/test_micromed_io.py
@@ -1,8 +1,9 @@
 """ Test example using version
 """
+
 from micromed_io import __version__
 
 
 def test_version():
     """Test version"""
-    assert __version__ == "0.4.2"
+    assert __version__ == "0.4.3"
